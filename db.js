9fcf500776d30347caf75ad2a10b2bde5701379c
vitoralkindar <vitoralkindar077@gmail.com>
2020-04-22 13:30:06 -0300

    meu primeiro commit

---
+++ db.js
@@ -0,0 +1,58 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./PJ.db')
+
+db.serialize(function() {
+    // Criar tabela
+//    db.run(`
+//        CREATE TABLE IF NOT EXISTS ideas(
+//            id INTEGER PRIMARY KEY AUTOINCREMENT,
+//            image TEXT,
+//            title TEXT,
+//            category TEXT,
+//            description TEXT,
+//            link TEXT
+//        );
+//    `)
+
+    // Inserir dado na tabela
+//    const query = `
+//    INSERT INTO ideas (
+//        image,
+//        title,
+//        category,
+//        description,
+//        link
+//
+//   ) VALUES (?,?,?,?,?);
+// `
+
+//const values =  [
+//    "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//    "Cursos de Programação",
+//    "Estudo",
+//    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae labore magni voluptates totam optio magnam aliquam, deleniti enim culpa, tenetur est. Sequi beatae magnam repellendus deserunt, amet ad iure aperiam!",
+//    "https://rocketseat.com.br"
+//]
+
+    //db.run(query, values, function(err) {
+    //    if (err) return console.log(err)
+
+    //    console.log(this)
+    //})
+
+    // Deletar um dado da tabela
+    //db.run (`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
+    //    if (err) return console.log(err)
+
+    //    console.log("DELETEI", this)
+    //})
+
+    // Consultar dados na tabela
+    //db.all(`SELECT * FROM ideas`, function(err, rows) {
+    //    if (err) return console.log(err)
+
+    //    console.log(rows)
+    //})
+})
+
+module.exports = db
